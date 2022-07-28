a8c1292a4353aa4e20fa48d200dacd9fad9eb904
Benedikt Eggers <be@spiegelburg.net>
2022-07-28 11:04:17 +0200

    OrganizationSiteInternalModel

--- src/Simplic.OxS.Organization.InternalClient/Model/OrganizationSiteInternalModel.cs
+++ src/Simplic.OxS.Organization.InternalClient/Model/OrganizationSiteInternalModel.cs
@@ -8,6 +8,9 @@ namespace Simplic.OxS.Organization.InternalClient.Model
 {
     public class OrganizationSiteInternalModel
     {
+        public Guid Id { get; set; }
+
+
         public string Name { get; set; }
 
         public AddressInternalModel Address { get; set; } = new AddressInternalModel();
