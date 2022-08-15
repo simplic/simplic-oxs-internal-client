8def8bb9b26f1c4e1a2eb3fdead2d23032deee15
MaxPatyk <max.patyk@web.de>
2022-08-15 13:49:51 +0200

    Add internal team client.

---
+++ src/Simplic.OxS.Organization.InternalClient/InternalTeamClient.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Simplic.OxS.InternalClient;
+using Simplic.OxS.Settings;
+using Simplic.OxS.Organization.InternalClient.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simplic.OxS.Organization.InternalClient
+{
+    public class InternalTeamClient : InternalClientBase
+    {
+        private readonly ILogger<InternalClientBase> logger;
+
+        public InternalTeamClient(IOptions<AuthSettings> settings, IRequestContext requestContext, ILogger<InternalClientBase> logger, IConfiguration configuration) : base(settings, requestContext, logger, configuration)
+        {
+            this.logger = logger;
+        }
+
+        public async Task<TeamInternalModel?> GetByUserId(Guid id)
+        {
+            try
+            {
+                return await Get<TeamInternalModel?>("organization", "Team", $"{id}");
+            }
+            catch (Exception ex)
+            {
+                logger.LogError($"Could make internal team call InternalTeamClient.Get/{id}", ex);
+                return null;
+                
+            }
+        }
+
+        public async Task<IList<TeamInternalModel>?> GetAllByUserId(Guid id)
+        {
+            try
+            {
+                return await Get<IList<TeamInternalModel>?>("organization", "Team", "get-all-by-userid", new Dictionary<string, string>
+                {
+                    { "id", id.ToString() }
+                });
+            }
+            catch (Exception ex)
+            {
+                logger.LogError("Could make internal team call InternalTeamClient.GetAllByUserId", ex);
+                throw;
+            }
+        }
+
+
+    }
+}
