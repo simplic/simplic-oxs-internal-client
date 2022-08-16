425ee88fb0717b2f10b111c66bc982da5a058167
Marvin Eichhoff <marvin.eichhoff@spiegelburg.biz>
2022-08-16 13:14:16 +0200

    add internal contact client

---
+++ src/Simplic.OxS.Contact.InternalClient/Model/AddressInternalModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simplic.OxS.Contact.InternalClient.Model
+{
+    public class AddressInternalModel
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? CompanyName { get; set; }
+        public string? Additional01 { get; set; }
+        public string? Additional02 { get; set; }
+        public string? Street { get; set; }
+        public string? HouseNumber { get; set; }
+        public string? Zipcode { get; set; }
+        public string? City { get; set; }
+        public string? CountryIso { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+    }
+}
---
+++ src/Simplic.OxS.Contact.InternalClient/Model/ContactInternalModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simplic.OxS.Contact.InternalClient.Model
+{
+    public class ContactInternalModel
+    {
+        public AddressInternalModel Address { get; set; } = new AddressInternalModel();
+
+        public Guid Id { get; set; }
+        public bool IsDeleted { get; set; }
+        public DateTime CreateDateTime { get; set; }
+        public Guid? CreateUserId { get; set; }
+        public string CreateUserName { get; set; }
+        public DateTime UpdateDateTime { get; set; }
+        public Guid? UpdateUserId { get; set; }
+        public string UpdateUserName { get; set; }
+        public Guid OrganizationId { get; set; }
+    }
+}
