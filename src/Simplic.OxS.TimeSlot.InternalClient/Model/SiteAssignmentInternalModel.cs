3159c7f155f5c470671ad4283dfe4c54eb000460
MaxPatyk <max.patyk@simplic.biz>
2023-12-27 14:06:50 +0100

    create site internal client.

---
+++ src/Simplic.OxS.TimeSlot.InternalClient/Model/SiteAssignmentInternalModel.cs
@@ -0,0 +1,18 @@
+﻿namespace Simplic.OxS.TimeSlot.InternalClient.Model
+{
+    /// <summary>
+    /// Represents the internal site assignment model.
+    /// </summary>
+    public class SiteAssignmentInternalModel
+    {
+        /// <summary>
+        /// Gets or sets the origanization id.
+        /// </summary>
+        public Guid? OrganizationId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the organization site id.
+        /// </summary>
+        public Guid? OrganizationSiteId { get; set; }
+    }
+}
