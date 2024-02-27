fda2fe002c62ec9ad80adb4c9d269906bec11812
4eck-qed <ssindustries@web.de>
2024-02-27 11:28:18 +0100

    fix name and add `GetFile`

---
+++ src/Simplic.OxS.CDN.InternalClient/InternalCDNClient.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Simplic.OxS.InternalClient;
+using Simplic.OxS.Settings;
+
+namespace Simplic.OxS.CDN.InternalClient;
+
+/// <summary>
+/// Internal client to interact with the CDN.
+/// </summary>
+public class InternalCDNClient : InternalClientBase
+{
+    private readonly ILogger logger;
+
+    public InternalCDNClient(
+        IOptions<AuthSettings> settings,
+        IRequestContext requestContext,
+        ILogger<InternalClientBase> logger,
+        IConfiguration configuration
+    )
+        : base(settings, requestContext, logger, configuration)
+    {
+        this.logger = logger;
+    }
+
+    /// <summary>
+    /// Gets a file by it's blob id.
+    /// </summary>
+    /// <param name="blobId">Id of the file</param>
+    /// <returns>Response containing the file stream and additional info</returns>
+    public async Task<GetFileResponse?> GetFile(Guid blobId)
+    {
+        try
+        {
+            return await Get<GetFileResponse>(
+                "cdn",
+                "InternalCDN",
+                "get-file",
+                new Dictionary<string, string> { { "blobId", $"{blobId}" } }
+            );
+        }
+        catch (Exception ex)
+        {
+            logger.LogError("Internal call failed {ex}", ex);
+            throw;
+        }
+    }
+
+    /// <summary>
+    /// Uploads bytes.
+    /// </summary>
+    /// <param name="request"></param>
+    /// <returns></returns>
+    public async Task<UploadInternalResponse?> UploadBytes(UploadBytesInternalRequest request)
+    {
+        try
+        {
+            return await Post<UploadInternalResponse, UploadBytesInternalRequest>(
+                "cdn",
+                "InternalCDN",
+                "upload",
+                request
+            );
+        }
+        catch (Exception ex)
+        {
+            logger.LogError("Internal call failed {ex}", ex);
+            throw;
+        }
+    }
+}
---
+++ src/Simplic.OxS.CDN.InternalClient/Model/GetFileResponse.cs
@@ -0,0 +1,22 @@
+﻿namespace Simplic.OxS.CDN.InternalClient;
+
+/// <summary>
+/// Response model for downloading a file.
+/// </summary>
+public class GetFileResponse
+{
+    /// <summary>
+    /// Gets or sets the file stream.
+    /// </summary>
+    public Stream FileStream { get; set; }
+    
+    /// <summary>
+    /// Gets or sets the content type.
+    /// </summary>
+    public string ContentType { get; set; }
+    
+    /// <summary>
+    /// Gets or sets the file download name.
+    /// </summary>
+    public string FileDownloadName { get; set; }
+}
