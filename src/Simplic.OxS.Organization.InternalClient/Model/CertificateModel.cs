e05be97b1ff98df11e5dfd1d3d35efc22e4c46a5
Benedikt Eggers <be@spiegelburg.net>
2024-12-29 01:26:22 +0100

    Add cert stuff

---
+++ src/Simplic.OxS.Organization.InternalClient/Model/CertificateModel.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simplic.OxS.Organization.InternalClient.Model
+{
+    /// <summary>
+    /// Represents a certificate
+    /// </summary>
+    public class CertificateModel
+    {
+        /// <summary>
+        /// Gets or sets the certiciate id
+        /// </summary>
+        public Guid Id { get; set; }
+
+        /// <summary>
+        /// Contains the certificate file
+        /// </summary>
+        public byte[] CertFile { get; set; }
+
+        /// <summary>
+        /// Gets or sets the certificate issuer
+        /// </summary>
+        public string Issuer { get; set; }
+
+        /// <summary>
+        /// Gets or sets the certificate subject
+        /// </summary>
+        public string Subject { get; set; }
+
+        /// <summary>
+        /// Gets or sets the from valid date
+        /// </summary>
+        public DateTime NotBefore { get; set; }
+
+        /// <summary>
+        /// Gets or sets the to valid date
+        /// </summary>
+        public DateTime NotAfter { get; set; }
+
+        /// <summary>
+        /// Gets or sets the serial number
+        /// </summary>
+        public string SerialNumber { get; set; }
+
+        /// <summary>
+        /// Gets or sets the thumbprint
+        /// </summary>
+        public string Thumbprint { get; set; }
+
+        /// <summary>
+        /// Gets or sets the create datetime
+        /// </summary>
+        public DateTime CreateDateTime { get; set; }
+
+        /// <summary>
+        /// Gets or sets the create userid
+        /// </summary>
+        public Guid? CreateUserId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the create username
+        /// </summary>
+        public string CreateUserName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the update datetime
+        /// </summary>
+        public DateTime UpdateDateTime { get; set; }
+
+        /// <summary>
+        /// Gets or sets the update user-id
+        /// </summary>
+        public Guid? UpdateUserId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the update username
+        /// </summary>
+        public string UpdateUserName { get; set; }
+    }
+}
