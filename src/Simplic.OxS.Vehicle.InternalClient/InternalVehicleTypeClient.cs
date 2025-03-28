d59817b5b4cdf83a402593f9632b9e7a4e814ade
MaxPatyk <max.patyk@simplic.biz>
2025-03-28 09:45:24 +0100

    Add internal vehicle type.

---
+++ src/Simplic.OxS.Vehicle.InternalClient/InternalVehicleTypeClient.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Simplic.OxS.InternalClient;
+using Simplic.OxS.Settings;
+using Simplic.OxS.Vehicle.InternalClient.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simplic.OxS.Vehicle.InternalClient;
+
+public class InternalVehicleTypeClient : InternalClientBase
+{
+    private readonly ILogger logger;
+
+    public InternalVehicleTypeClient(IOptions<AuthSettings> settings, IRequestContext requestContext, ILogger<InternalClientBase> logger, IConfiguration configuration) : base(settings, requestContext, logger, configuration)
+    {
+        this.logger = logger;
+    }
+
+    /// <summary>
+    /// Gets a vehicle type by id.
+    /// </summary>
+    /// <param name="id">Vehicle type id</param>
+    /// <returns></returns>
+    public async Task<VehicleTypeInternalModel?> Get(Guid id)
+    {
+        try
+        {
+            return await Get<VehicleTypeInternalModel?>("vehicleType", "InternalVehicleType", "",
+                new Dictionary<string, string>
+                {
+                        { "id", id.ToString() }
+                });
+        }
+        catch (Exception ex)
+        {
+            logger.LogError("Internal call failed", ex);
+            throw;
+        }
+    }
+}
---
+++ src/Simplic.OxS.Vehicle.InternalClient/Model/Type/VehicleSubTypeInternalModel.cs
@@ -0,0 +1,27 @@
+﻿namespace Simplic.OxS.Vehicle.InternalClient;
+
+/// <summary>
+/// Represents an internal vehicle sub type model.
+/// </summary>
+public class VehicleSubTypeInternalModel
+{
+    /// <summary>
+    /// Gets or sets the sub type id.
+    /// </summary>
+    public Guid Id { get; set; }
+
+    /// <summary>
+    /// Gets or sets the sub type name.
+    /// </summary>
+    public string Name { get; set; }
+
+    /// <summary>
+    /// Gets or sets the display name.
+    /// </summary>
+    public string DisplayName { get; set; }
+
+    /// <summary>
+    /// Gets or sets the alias name from webfleet or others.
+    /// </summary>
+    public string? Alias { get; set; }
+}
