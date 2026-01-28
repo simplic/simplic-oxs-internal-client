133a04db1808bbaa8dd0b90935a2d7d8ddd0b1fa
Benedikt Eggers <be@spiegelburg.net>
2026-01-28 11:23:39 +0100

    Add internal department controller

---
+++ src/Simplic.OxS.Logistics.InternalClient/InternalDepartmentClient.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Simplic.OxS.InternalClient;
+using Simplic.OxS.Logistics.InternalClient.Model;
+using Simplic.OxS.Settings;
+
+namespace Simplic.OxS.Logistics.InternalClient
+{
+    /// <summary>
+    /// Internal client to communicate with the internal loading aid type api.
+    /// </summary>
+    public class InternalDepartmentClient : InternalClientBase
+    {
+        private readonly ILogger<InternalClientBase> logger;
+
+        /// <summary>
+        /// Initializes a new instance of <see cref="InternalDepartmentClient"/> with dependency injection.
+        /// </summary>
+        public InternalDepartmentClient(IOptions<AuthSettings> settings,
+                                          IRequestContext requestContext,
+                                          ILogger<InternalClientBase> logger,
+                                          IConfiguration configuration)
+            : base(settings, requestContext, logger, configuration)
+        {
+            this.logger = logger;
+        }
+
+        /// <summary>
+        /// Gets an loading aid type by id.
+        /// </summary>
+        /// <param name="id">The loading aid type id.</param>
+        public async Task<DepartmentResponse?> Get(Guid id)
+        {
+            try
+            {
+                return await Get<DepartmentResponse?>("logistics", "InternalDepartment", "get-by-id",
+                    new Dictionary<string, string>
+                    {
+                        { "id", $"{id}" }
+                    });
+            }
+            catch (Exception ex)
+            {
+                logger.LogError("Could not make internal department type call InternalDepartmentClient.GetById", ex);
+                throw;
+            }
+        }
+    }
+}
---
+++ src/Simplic.OxS.Logistics.InternalClient/Model/DepartmentResponse.cs
@@ -0,0 +1,27 @@
+﻿namespace Simplic.OxS.Logistics.InternalClient.Model;
+
+/// <summary>
+/// Response model for <see cref="Department"/>.
+/// </summary>
+public class DepartmentResponse
+{
+    /// <summary>
+    /// Gets or sets the id.
+    /// </summary>
+    public Guid Id { get; set; }
+
+    /// <summary>
+    /// Gets or sets the name.
+    /// </summary>
+    public string Name { get; set; } = "";
+
+    /// <summary>
+    /// Gets or sets the order id.
+    /// </summary>
+    public int OrderId { get; set; }
+
+    /// <summary>
+    /// Gets or sets the hex color.
+    /// </summary>
+    public string HexColor { get; set; } = "";
+}
