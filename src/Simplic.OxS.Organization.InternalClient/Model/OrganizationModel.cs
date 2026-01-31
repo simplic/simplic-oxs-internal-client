390a2d646fbe3c880058dfcd83304bba4906aea7
Benedikt Eggers <be@spiegelburg.net>
2026-01-31 20:59:03 +0100

    Add EntraTenantId, EntraIdAdminConsent

--- src/Simplic.OxS.Organization.InternalClient/Model/OrganizationModel.cs
+++ src/Simplic.OxS.Organization.InternalClient/Model/OrganizationModel.cs
@@ -24,5 +24,18 @@
         /// Gets or sets the billing-address of organization
         /// </summary>
         public BillingAddressModel BillingAddressModel { get; set; }
+
+        /// <summary>
+        /// Gets or set the entra tenant id
+        /// </summary>
+        public string? EntraTenantId { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether admin consent has been granted for the application in Azure Active
+        /// Directory.
+        /// </summary>
+        /// <remarks>This property reflects the admin consent status, which is essential for determining
+        /// if the application can access certain resources on behalf of users.</remarks>
+        public bool EntraIdAdminConsent { get; set; }
     }
 }
