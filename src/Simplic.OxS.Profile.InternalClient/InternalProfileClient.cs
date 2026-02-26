1403e53525be32f64f469c623a06ff51e54486fb
Benedikt Eggers <be@spiegelburg.net>
2026-02-26 13:57:51 +0100

    Add profile client

---
+++ src/Simplic.OxS.Profile.InternalClient/InternalProfileClient.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Simplic.OxS.InternalClient;
+using Simplic.OxS.Profile.InternalClient.Model;
+using Simplic.OxS.Settings;
+
+namespace Simplic.OxS.Profile.InternalClient
+{
+    public class InternalProfileClient : InternalClientBase
+    {
+        private readonly ILogger<InternalClientBase> logger;
+
+        /// <summary>
+        /// Initializes a new instance of <see cref="InternalProfileClient"/> with dependency injection.
+        /// </summary>
+        public InternalProfileClient(IOptions<AuthSettings> settings,
+                                          IRequestContext requestContext,
+                                          ILogger<InternalClientBase> logger,
+                                          IConfiguration configuration)
+            : base(settings, requestContext, logger, configuration)
+        {
+            this.logger = logger;
+        }
+
+        /// <summary>
+        /// Gets the profile settings for the current user by making an internal API call to the "profile" endpoint.
+        /// </summary>
+        /// <returns>Profile</returns>
+        public async Task<ProfileSettingsModel?> Get()
+        {
+            try
+            {
+                return await Get<ProfileSettingsModel?>("profile", "InternalProfile", "",
+                    new Dictionary<string, string>
+                    {
+
+                    });
+            }
+            catch (Exception ex)
+            {
+                logger.LogError("Could not make internal profile call InternalProfileClient.Get", ex);
+                throw;
+            }
+        }
+    }
+}
---
+++ src/Simplic.OxS.Profile.InternalClient/Model/ProfileSettingsModel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simplic.OxS.Profile.InternalClient.Model;
+
+public class ProfileSettingsModel
+{
+    public UserProfileModel UserProfile { get; set; }
+    public AppSettings AppSettings { get; set; }
+
+    /// <summary>
+    /// Gets or sets wherther the user wants to receive newsletter mails.
+    /// </summary>
+    public bool ReceiveNewsletter { get; set; }
+
+    /// <summary>
+    /// Gets or sets a property to indicate whether the user has finished the setup wizard.
+    /// </summary>
+    public bool? FinishedSetupWizard { get; set; }
+
+    /// <summary>
+    /// Get or sets a property to indicate whether the user has finished the tour guide.
+    /// </summary>
+    public bool? FinishedTourGuide { get; set; }
+}
+
+public class UserProfileModel
+{
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public string? PhoneNumber { get; set; }
+    public string? AboutMe { get; set; }
+    public string? Thumbnail { get; set; }
+}
+
+public class AppSettings
+{
+    public GlobalAppSettings Global { get; set; } = new();
+}
+
+public class GlobalAppSettings
+{
+    /// <summary>
+    /// Gets or sets the language.<br/>
+    /// Common abbreviation such as 'en' or 'de'.<br/>
+    /// Defaults to 'de'.
+    /// </summary>
+    public string Language { get; set; } = "de";
+}
