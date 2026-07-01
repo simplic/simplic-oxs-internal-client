65621db514213adff4c5837ea37eb60e60c53c8c
juliusfunke <contact@juliusfunke.com>
2026-07-01 15:06:18 +0200

    add create endpoint to internal document client

---
+++ src/Simplic.OxS.Document.InternalClient.V2/Model/CreateDocumentInternalRequest.cs
@@ -0,0 +1,53 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Simplic.OxS.Document.InternalClient.V2.Model;
+
+/// <summary>
+/// Request model for posting a <see cref="Document"/>.
+/// </summary>
+public class CreateDocumentInternalRequest
+{
+    /// <summary>
+    /// Gets or sets the file name.
+    /// </summary>
+    [Required]
+    public string? Name { get; set; }
+
+    /// <summary>
+    /// Gets or sets the datetime.
+    /// </summary>
+    public DateTime DateTime { get; set; }
+
+    /// <summary>
+    /// Gets or sets the version.
+    /// </summary>
+    [Required]
+    public PostDocumentVersionRequest? Version { get; set; }
+
+    /// <summary>
+    /// Gets or sets the reference.
+    /// </summary>
+    public string? Reference { get; set; }
+
+    /// <summary>
+    /// Gets or sets the bar code.
+    /// </summary>
+    public string? Barcode { get; set; }
+
+    public Guid? PrimaryClassificationId { get; set; }
+
+    /// <summary>
+    /// Gets or sets the raw data url. 
+    /// </summary>
+    public string? RawDataUrl { get; set; }
+
+    /// <summary>
+    /// Get or set the raw data provider
+    /// </summary>
+    public string? RawDataProvider { get; set; }
+
+    /// <summary>
+    /// Gets or sets a list od documents paths to add
+    /// </summary>
+    public IList<AddDocumentPathRequest>? Paths { get; set; } = [];
+}
